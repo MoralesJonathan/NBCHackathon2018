dd11f3804199296f0135d90f038a248131b1730b
Jonathan Morales <jonmorazav@yahoo.com>
2018-11-04 04:55:03 -0500

    pulling bills with api

--- client/src/Pages/LandingPage.js
+++ client/src/Pages/LandingPage.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from '../Components/Navbar/Navbar';
+import axios from "axios";
 import './LandingPage.css'
 
 class LandingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            bills : [
-                {title: 'Pepe title number 1', billID: '1', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro accusamus debitis a, minus praesentium cupiditate repellat excepturi magni? Pariatur in architecto cupiditate? Dicta rem ipsa placeat culpa voluptatibus exercitationem.'},
-                {title: 'Pepe title number 2', billID: '2', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro accusamus debitis a, minus praesentium cupiditate repellat excepturi magni? Pariatur in architecto cupiditate? Dicta rem ipsa placeat culpa voluptatibus exercitationem.'},
-                {title: 'Pepe title number 3', billID: '3', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro accusamus debitis a, minus praesentium cupiditate repellat excepturi magni? Pariatur in architecto cupiditate? Dicta rem ipsa placeat culpa voluptatibus exercitationem.'},
-                {title: 'Pepe title number 2', billID: '4', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro accusamus debitis a, minus praesentium cupiditate repellat excepturi magni? Pariatur in architecto cupiditate? Dicta rem ipsa placeat culpa voluptatibus exercitationem.'},
-                {title: 'Pepe title number 3', billID: '5', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti porro accusamus debitis a, minus praesentium cupiditate repellat excepturi magni? Pariatur in architecto cupiditate? Dicta rem ipsa placeat culpa voluptatibus exercitationem.'}
-            ]
+            featuredBill: {},
+            bills : []
         }
     } 
     componentDidMount(){
-        //make cool api call
-        //save to state
+        axios.post("/api/ballot/issues/",  { state: 'fl', issues: [ 'guns', 'healthcare', 'education' ] })
+        .then( (response) => {
+            console.log(response)
+            this.setState({featuredBill:response.data[0], bills : response.data.splice(1)})
+          })
+          .catch((error) => {
+            console.log(error);
+          });
     }
     createCard(data){
-        return (<div class="col-md-4 col-12"><div class="card"> <div class="card-body"><h1 class="card-title text-primary">{data.title}</h1> <h3 class="card-subtitle mb-2 text-muted">Community bill</h3> <p class="card-text">{data.description}</p><div class="d-none d-md-block"> <button type="button" class="btn btn-outline-secondary">Ignore</button> <a href={"/bill/"}><button type="button" class="btn btn-outline-takeAction">Take Action</button> </a></div></div></div></div>);
+        return (<div className="col-md-4 col-12"><div className="card"> <div className="card-body"><h1 className="card-title text-primary">{data.title}</h1> <h3 className="card-subtitle mb-2 text-muted">Community bill</h3> <p className="card-text">{data.description}</p><div className="d-none d-md-block"> <button type="button" className="btn btn-outline-secondary">Ignore</button> <a href={"/bill/"}><button type="button" className="btn btn-outline-takeAction">Take Action</button> </a></div></div></div></div>);
     }
     loadCards() {
         let cards = [];
@@ -32,12 +34,12 @@ class LandingPage extends Component {
             }
             else {
                 cards.push(this.createCard(bills[x]));
-                grid.push(<div class="row">{cards}</div>)
+                grid.push(<div className="row">{cards}</div>)
                 cards = [];
             }
         }
         if(cards.length > 0){
-            grid.push(<div class="row">{cards}</div>)
+            grid.push(<div className="row">{cards}</div>)
         }
         return grid
     }
@@ -46,26 +48,26 @@ class LandingPage extends Component {
             <div>
                 <Navbar/>
                 <div id="landingPageCard">
-                    <div class="card" style={{borderRadius:'0px'}}>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-8 col-12">
-                                    <h1 class="card-title text-featured">Super cool tax reform bill</h1>
-                                    <h3 class="card-subtitle mb-2 text-subfeatured">Community bill</h3>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis hendrerit mattis. Aliquam rhoncus dapibus ultrices. Nam bibendum auctor massa, vitae consectetur odio accumsan a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis hendrerit mattis. Aliquam rhoncus dapibus ultrices. </p>
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-md-8 col-12">
+                                    <h1 className="card-title text-featured">{this.state.featuredBill.title}</h1>
+                                    <h3 className="card-subtitle mb-2 text-subfeatured">Community bill</h3>
+                                    <p className="card-text">{this.state.featuredBill.summary}</p>
                                 </div>
-                                <div class="col-md-4 d-none d-sm-flex">
+                                <div className="col-md-4 d-none d-sm-flex">
                                     <img id="featuredImage" src="https://via.placeholder.com/400x200"></img>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="container" id="cards-container">
+                <div className="container" id="cards-container">
                     {this.loadCards()}
                 </div>
                 <footer>
-                    <div class="container">
+                    <div className="container">
                     <span>Copyright por lo menos</span>
                     <span style={{float: 'right'}}>Ya tu sabes inc</span>
                     </div>
