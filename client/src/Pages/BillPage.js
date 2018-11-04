93518fcbecb98258060170414487d4925e23aa89
Jonathan Morales <jonmorazav@yahoo.com>
2018-11-04 11:38:58 -0500

    pass title through localstorage

--- client/src/Pages/BillPage.js
+++ client/src/Pages/BillPage.js
@@ -12,9 +12,9 @@ class BillPage extends Component {
             loading: true,
             pdf: '',
             pageNumber: 1,
-            title: "Make hackathons great again",
+            title: "",
             billNumber: this.props.match.params.id,
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet cursus massa. Aliquam commodo at nisi sed facilisis. Suspendisse varius nulla id maximus convallis. Fusce in luctus metus. Proin et ipsum ut lacus euismod dapibus. Nam rhoncus, turpis vel sodales ornare, arcu magna tempus est, laoreet ullamcorper dolor justo et dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed pulvinar in enim eget tempor. Morbi urna velit, feugiat id erat nec, viverra tempor lorem. Vestibulum in vehicula odio, et dapibus augue. Cras ornare mauris at interdum mattis. Curabitur posuere volutpat lobortis. Sed tempus sapien pretium, pharetra lectus eu, molestie nunc. Maecenas euismod diam eu viverra luctus.\nCurabitur vitae pulvinar lacus. Sed dictum massa id ligula rutrum consectetur. Cras sit amet urna nibh. Suspendisse at auctor nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ullamcorper ante libero, nec faucibus odio consectetur vel. Aliquam commodo faucibus tortor vitae sodales. Nulla sit amet leo ut eros tristique varius a vel nibh.\nDonec a felis sit amet tortor venenatis bibendum. Sed gravida elit sit amet metus feugiat efficitur. Integer sed facilisis massa. Aenean sollicitudin aliquet erat, sit amet mattis tellus iaculis nec. Duis felis dolor, lobortis at scelerisque quis, rutrum ac augue. Maecenas lobortis nec neque in ornare. In aliquet lorem non eros faucibus bibendum. Morbi semper eros est, a blandit arcu auctor et. Donec cursus blandit convallis. Curabitur cursus imperdiet sem, ut hendrerit tellus dictum et. Aenean vestibulum dui hendrerit bibendum dapibus. Suspendisse potenti. Nunc varius erat sit amet vestibulum pharetra. Quisque id volutpat lacus. Suspendisse efficitur magna et orci tempor, sed viverra erat euismod."
+            description: ""
         }
     }
     componentDidMount() {
@@ -33,7 +33,8 @@ class BillPage extends Component {
                     () => {
                         axios.get(`/api/ballot/${this.props.match.params.id}`)
                             .then(response => {
-                                this.setState({ pdf: response.data.pdf, loading: false });
+                                let billTitle = localStorage.getItem('billTitle');
+                                this.setState({ pdf: response.data.pdf, loading: false, title: billTitle});
                             })
                     })
             })
